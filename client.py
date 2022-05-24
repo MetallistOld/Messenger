fdbb49db6219a0ee9731170111af6f7313f7167f
Anatoliy Maximichev <maximichev@mail.ru>
2022-05-24 17:03:07 +0300

    Сделал ДЗ_3

---
+++ client.py
@@ -0,0 +1,77 @@
+"""Программа-клиент"""
+
+import sys
+import json
+import socket
+import time
+from common.variables import ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME, \
+    RESPONSE, ERROR, DEFAULT_IP_ADDRESS, DEFAULT_PORT
+from common.utils import get_message, send_message
+
+
+def create_presence(account_name='Guest'):
+    """
+    Функция генерирует запрос о присутствии клиента
+    :param account_name:
+    :return:
+    """
+    # {'action': 'presence', 'time': 1573760672.167031, 'user': {'account_name': 'Guest'}}
+    out = {
+        ACTION: PRESENCE,
+        TIME: time.time(),
+        USER: {
+            ACCOUNT_NAME: account_name
+        }
+    }
+    return out
+
+
+def process_ans(message):
+    """
+    Функция разбирает ответ сервера
+    :param message:
+    :return:
+    """
+    if RESPONSE in message:
+        if message[RESPONSE] == 200:
+            return '200 : OK'
+        return f'400 : {message[ERROR]}'
+    raise ValueError
+
+
+def main():
+    """Загружаем параметры командной строки"""
+    # client.py 192.168.0.100 8079
+    try:
+        server_address = sys.argv[1]
+        # print("address", server_address)
+        server_port = int(sys.argv[2])
+        # print(server_port)
+        if server_port < 1024 or server_port > 65535:
+            raise ValueError
+    except IndexError:
+        server_address = DEFAULT_IP_ADDRESS
+        server_port = DEFAULT_PORT
+    except ValueError:
+        print('В качестве порта может быть указано только число в диапазоне от 1024 до 65535.')
+        sys.exit(1)
+
+    # Инициализация сокета и обмен
+    try:
+        # print("address", server_address)
+        # print(server_port)
+        transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        transport.connect((server_address, server_port))
+        message_to_server = create_presence()
+        send_message(transport, message_to_server)
+        answer = process_ans(get_message(transport))
+        print(answer)
+    except ConnectionRefusedError:  # Если сервер не запущен, выдать предупреждение,а не вылетит с ошибкой
+        print('Сервер не отвечает')
+        sys.exit(1)
+    except (ValueError, json.JSONDecodeError):
+        print('Не удалось декодировать сообщение сервера.')
+
+
+if __name__ == '__main__':
+    main()
