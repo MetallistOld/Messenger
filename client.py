0cce0e7d9207bd186a402d5df3ca4c82f5342935
Anatoliy Maximichev <84544880+MetallistOld@users.noreply.github.com>
2022-06-15 22:44:36 +0300

    Сделал ДЗ-7 (#7)

--- client.py
+++ client.py
@@ -7,13 +7,45 @@ import time
 import logging
 import logs.config_client_log
 from common.variables import ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME, \
-    RESPONSE, ERROR, DEFAULT_IP_ADDRESS, DEFAULT_PORT
+    RESPONSE, ERROR, DEFAULT_IP_ADDRESS, DEFAULT_PORT, SENDER, MESSAGE, MESSAGE_TEXT
 from common.utils import get_message, send_message
 import argparse
-from errors import ReqFieldMissingError
+from errors import ReqFieldMissingError, ServerError
 from decos import log
 
 LOGGER = logging.getLogger('client')
+
+
+@log
+def message_from_server(message):
+    """Функция - обработчик сообщений других пользователей, поступающих с сервера"""
+    if ACTION in message and message[ACTION] == MESSAGE and SENDER in message and MESSAGE_TEXT in message:
+        print(f'Получено сообщение от пользователя '
+              f'{message[SENDER]}:\n{message[MESSAGE_TEXT]}')
+        LOGGER.info(f'Получено сообщение от пользователя '
+                    f'{message[SENDER]}:\n{message[MESSAGE_TEXT]}')
+    else:
+        LOGGER.error(f'Получено некорректное сообщение с сервера: {message}')
+
+
+@log
+def create_message(sock, account_name='Guest'):
+    """Функция запрашивает текст сообщения и возвращает его.
+    Так же завершает работу при вводе команды
+    """
+    message = input('Введите сообщение для отправки или \'!!!\' для завершения работы: ')
+    if message == '!!!':
+        sock.close()
+        LOGGER.info('Завершение работы по команде пользователя.')
+        sys.exit(0)
+    message_dict = {
+        ACTION: MESSAGE,
+        TIME: time.time(),
+        ACCOUNT_NAME: account_name,
+        MESSAGE_TEXT: message
+    }
+    LOGGER.debug(f'Сформирован словарь сообщения: {message_dict}')
+    return message_dict
 
 
 @log
@@ -56,17 +88,11 @@ def create_arg_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('addr', default=DEFAULT_IP_ADDRESS, nargs='?')
     parser.add_argument('port', default=DEFAULT_PORT, type=int, nargs='?')
-    return parser
-
-
-def main():
-    """Загружаем параметры командной строки"""
-    # client.py 192.168.0.100 8079
-    parser = create_arg_parser()
+    parser.add_argument('-m', '--mode', default='listen', nargs='?')
     namespace = parser.parse_args(sys.argv[1:])
     server_address = namespace.addr
     server_port = namespace.port
-
+    client_mode = namespace.mode
     # проверим подходящий номер порта
     if not 1023 < server_port < 65536:
         LOGGER.critical(f'Запуск клиента с не правильным номером порта: {server_port}. '
@@ -75,6 +101,24 @@ def main():
 
     LOGGER.info(f'Запущен клиент с парамерами: адрес сервера: '
                 f'{server_address}, порт: {server_port}')
+
+    # Проверим допустим ли выбранный режим работы клиента
+    if client_mode not in ('listen', 'send'):
+        LOGGER.critical(f'Указан недопустимый режим работы {client_mode}, '
+                        f'допустимые режимы: listen , send')
+        sys.exit(1)
+
+    return server_address, server_port, client_mode
+
+
+def main():
+    """Загружаем параметры командной строки"""
+    # client.py 192.168.0.100 8079
+    server_address, server_port, client_mode = create_arg_parser()
+
+    LOGGER.info(
+        f'Запущен клиент: адрес сервера: {server_address}, '
+        f'порт: {server_port}, режим работы: {client_mode}')
 
     # Инициализация сокета и обмен
     try:
@@ -95,6 +139,32 @@ def main():
         LOGGER.error('Не удалось декодировать полученную Json строку.')
     except ReqFieldMissingError as missing_error:
         LOGGER.error(f'В ответе сервера отсутствует необходимое поле {missing_error.missing_field}')
+    except ServerError as error:
+        LOGGER.error(f'При установке соединения сервер вернул ошибку: {error.text}')
+        sys.exit(1)
+    else:
+        # Если соединение с сервером установлено корректно, начинаем обмен с ним, согласно требуемому режиму.
+
+        if client_mode == 'send':
+            print('Режим работы - отправка сообщений.')
+        else:
+            print('Режим работы - приём сообщений.')
+        while True:
+            # режим работы - отправка сообщений
+            if client_mode == 'send':
+                try:
+                    send_message(transport, create_message(transport))
+                except (ConnectionResetError, ConnectionError, ConnectionAbortedError):
+                    LOGGER.error(f'Соединение с сервером {server_address} разорвано.')
+                    sys.exit(1)
+
+            # Режим работы приём:
+            if client_mode == 'listen':
+                try:
+                    message_from_server(get_message(transport))
+                except (ConnectionResetError, ConnectionError, ConnectionAbortedError):
+                    LOGGER.error(f'Соединение с сервером {server_address} разорвано.')
+                    sys.exit(1)
 
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -6,16 +6,18 @@ import logging
 import logs.config_server_log
 from errors import IncorrectDataRecivedError
 from common.variables import ACTION, ACCOUNT_NAME, RESPONSE, MAX_CONNECTIONS, \
-    PRESENCE, TIME, USER, ERROR, DEFAULT_PORT, DEFAULT_IP_ADDRESS
+    PRESENCE, TIME, USER, ERROR, DEFAULT_PORT, DEFAULT_IP_ADDRESS, SENDER, MESSAGE_TEXT, MESSAGE
 from common.utils import get_message, send_message
 import argparse
+import select
+import time
 from decos import log
 
 LOGGER = logging.getLogger('server')
 
 
 @log
-def process_client_message(message):
+def process_client_message(message, messages_list, client):
     """
     Обработчик сообщений от клиентов, принимает словарь -
     сообщение от клинта, проверяет корректность,
@@ -27,11 +29,19 @@ def process_client_message(message):
     LOGGER.debug(f'Разбор сообщения от клиента : {message}')
     if ACTION in message and message[ACTION] == PRESENCE and TIME in message \
             and USER in message and message[USER][ACCOUNT_NAME] == 'Guest':
-        return {RESPONSE: 200}
-    return {
-        RESPONSE: 400,
-        ERROR: 'Bad Request'
-    }
+        send_message(client, {RESPONSE: 200})
+        return
+    # Если это сообщение, то добавляем его в очередь сообщений.
+    elif ACTION in message and message[ACTION] == MESSAGE and \
+            TIME in message and MESSAGE_TEXT in message:
+        messages_list.append((message[ACCOUNT_NAME], message[MESSAGE_TEXT]))
+        return
+    else:
+        send_message(client, {
+            RESPONSE: 400,
+            ERROR: 'Bad Request'
+        })
+        return
 
 
 @log
@@ -43,7 +53,7 @@ def create_arg_parser():
     return parser
 
 
-def main(listen_address=None):
+def main():
     """
     Загрузка параметров командной строки, если нет параметров, то задаём значения по умолчанию.
     Сначала обрабатываем порт:
@@ -51,6 +61,7 @@ def main(listen_address=None):
     :return:
     """
 
+    global client
     """Загрузка параметров командной строки, если нет параметров, то задаём значения по умолчанию"""
     parser = create_arg_parser()
     namespace = parser.parse_args(sys.argv[1:])
@@ -71,30 +82,59 @@ def main(listen_address=None):
 
     transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     transport.bind((listen_address, listen_port))
+    transport.settimeout(0.5)
+
+    # список клиентов , очередь сообщений
+    clients = []
+    messages = []
 
     # Слушаем порт
 
     transport.listen(MAX_CONNECTIONS)
 
     while True:
-        client, client_address = transport.accept()
-        LOGGER.info(f'Установлено соединение с ПК {client_address}')
         try:
-            message_from_client = get_message(client)
-            LOGGER.debug(f'Получено сообщение {message_from_client}')
-            response = process_client_message(message_from_client)
-            LOGGER.info(f'Сформирован ответ клиенту {response}')
-            send_message(client, response)
-            LOGGER.debug(f'Соединение с клиентом {client_address} закрывается.')
-            client.close()
-        except (ValueError, json.JSONDecodeError):
-            LOGGER.error(f'От клиента {client_address} получена не корректная Json строка . '
-                         f'Соединение закрывается.')
-            client.close()
-        except IncorrectDataRecivedError:
-            LOGGER.error(f'От клиента {client_address} приняты некорректные данные. '
-                         f'Соединение закрывается.')
-            client.close()
+            client, client_address = transport.accept()
+        except OSError:
+            pass
+        else:
+            LOGGER.info(f'Установлено соединение с ПК {client_address}')
+            clients.append(client)
+
+        recv_data_lst = []
+        send_data_lst = []
+        err_lst = []
+        try:
+            if clients:
+                recv_data_lst, send_data_lst, err_lst = select.select(clients, clients, [], 0)
+        except OSError:
+            pass
+
+        # принимаем сообщения и если там есть сообщения,
+        # кладём в словарь, если ошибка, исключаем клиента.
+        if recv_data_lst:
+            for client_with_message in recv_data_lst:
+                try:
+                    process_client_message(get_message(client_with_message), messages, client_with_message)
+                except:
+                    LOGGER.info(f'Клиент {client_with_message.getpeername()} отключился от сервера.')
+                    clients.remove(client_with_message)
+
+        # Если есть сообщения для отправки и ожидающие клиенты, отправляем им сообщение.
+        if messages and send_data_lst:
+            message = {
+                ACTION: MESSAGE,
+                SENDER: messages[0][0],
+                TIME: time.time(),
+                MESSAGE_TEXT: messages[0][1]
+            }
+            del messages[0]
+            for waiting_client in send_data_lst:
+                try:
+                    send_message(waiting_client, message)
+                except:
+                    LOGGER.info(f'Клиент {waiting_client.getpeername()} отключился от сервера.')
+                    clients.remove(waiting_client)
 
 
 if __name__ == '__main__':
