d4e0c430899f854d5ab680edea99a79a1ac71280
Anatoliy Maximichev <84544880+MetallistOld@users.noreply.github.com>
2022-07-03 20:25:23 +0300

    Сделал ДЗ-8 (#8)

--- launcher.py
+++ launcher.py
@@ -14,10 +14,7 @@ while True:
         PROCESS.append(subprocess.Popen('python server.py',
                                         creationflags=subprocess.CREATE_NEW_CONSOLE))
         for i in range(2):
-            PROCESS.append(subprocess.Popen('python client.py -m send',
-                                            creationflags=subprocess.CREATE_NEW_CONSOLE))
-        for i in range(5):
-            PROCESS.append(subprocess.Popen('python client.py -m listen',
+            PROCESS.append(subprocess.Popen('python client.py -n user{i+1}',
                                             creationflags=subprocess.CREATE_NEW_CONSOLE))
     elif ACTION == 'x':
         while PROCESS:
